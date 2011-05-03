829fecfa20c500d7c3e50a80f5d7f889bab3a0dd
Vincent Hanquez <vincent@snarc.org>
2011-05-03 20:41:47 +0100

    cleanup the generate number interface. add a generateBetween as equivalent of randomR for integer

--- Number/Generate.hs
+++ Number/Generate.hs
@@ -1,22 +1,24 @@
 module Number.Generate
 	( generateMax
+	, generateBetween
 	, generateOfSize
 	) where
 
 import Number.Serialize
 import Crypto.Random
 import qualified Data.ByteString as B
+import Data.Bits ((.|.))
 
-{- a bit too simplitic and probably not very good. need to have a serious look
- - on how to generate random integer. -}
+-- | generate a positive integer between 0 and m.
+-- using as many bytes as necessary to the same size as m, that are converted to integer.
 generateMax :: CryptoRandomGen g => g -> Integer -> Either GenError (Integer, g)
-generateMax rng m =
-	let nbbytes = nbBytes m in
-	case genBytes nbbytes rng of
-		Left err         -> Left err
-		Right (bs, rng') ->
-			let n = os2ip bs in
-			if n < m then Right (n, rng') else generateMax rng' m
+generateMax rng m = genBytes (logiBytes m) rng >>= \(bs, rng') -> return (os2ip bs `mod` m, rng')
+
+-- | generate a number between the inclusive bound [low,high].
+generateBetween :: CryptoRandomGen g => g -> Integer -> Integer -> Either GenError (Integer, g)
+generateBetween rng low high = generateMax rng rmax >>= \(v, rng') -> return (low + v, rng')
+	where
+		rmax = high - low + 1 -- relative maximum before being corrected by the low bound
 
 -- | generate a positive integer of a specific size in bits.
 -- the number of bits need to be multiple of 8. It will always returns
@@ -26,7 +28,7 @@ generateOfSize rng bits = case genBytes (bits `div` 8) rng of
 	Left err         -> Left err
 	Right (bs, rng') -> Right (os2ip $ snd $ B.mapAccumL (\acc w -> (0, w .|. acc)) 0xc0 bs, rng')
 
-nbBytes :: Integer -> Int
-nbBytes n
+logiBytes :: Integer -> Int
+logiBytes n
 	| n < 256   = 1
-	| otherwise = 1 + nbBytes (n `div` 256)
+	| otherwise = 1 + logiBytes (n `div` 256)
