672195da30611ee38c69c813971cb7636a7c7e40
Vincent Hanquez <vincent@snarc.org>
2013-01-11 08:29:54 +0000

    add some documentation direct links, as both modules documentation are empty with haddock.

--- Crypto/Cipher/AES.hs
+++ Crypto/Cipher/AES.hs
@@ -5,6 +5,13 @@
 -- Maintainer  : Vincent Hanquez <vincent@snarc.org>
 -- Stability   : experimental
 -- Portability : Good
+--
+-- This module just re-export Crypto.Cipher.AES from the
+-- cipher-aes module.
+--
+-- Documentation can be found at
+-- <http://hackage.haskell.org/package/cipher-aes>
+--
 
 module Crypto.Cipher.AES
 	( module Crypto.Cipher.AES
--- Crypto/Cipher/RC4.hs
+++ Crypto/Cipher/RC4.hs
@@ -5,6 +5,12 @@
 -- Stability   : experimental
 -- Portability : Good
 --
+-- This module just re-export Crypto.Cipher.RC4 from the
+-- cipher-rc4 module.
+--
+-- Documentation can be found at
+-- <http://hackage.haskell.org/package/cipher-rc4>
+--
 
 {-# LANGUAGE PackageImports #-}
 module Crypto.Cipher.RC4 (module Crypto.Cipher.RC4) where
