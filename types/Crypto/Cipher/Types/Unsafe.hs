ae69ccd6cc109e03ac90e6e319f93ec061077ef5
Vincent Hanquez <vincent@snarc.org>
2013-10-24 11:13:06 +0800

    add description of unsafe module.

--- types/Crypto/Cipher/Types/Unsafe.hs
+++ types/Crypto/Cipher/Types/Unsafe.hs
@@ -1,3 +1,13 @@
+-- |
+-- Module      : Crypto.Cipher.Types.Unsafe
+-- License     : BSD-style
+-- Maintainer  : Vincent Hanquez <vincent@snarc.org>
+-- Stability   : Stable
+-- Portability : Excellent
+--
+-- mutable and unsafe interface for Block ciphers.
+-- export a BlockCipherIO class
+--
 module Crypto.Cipher.Types.Unsafe
     ( BlockCipherIO(..)
     , BufferLength
