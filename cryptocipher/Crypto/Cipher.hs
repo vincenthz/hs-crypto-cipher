f3a1e26b91d994122f163bf64135a545e98144c0
Vincent Hanquez <vincent@snarc.org>
2013-10-25 09:02:00 +0800

    add simplified example in header of crypto.cipher.

--- cryptocipher/Crypto/Cipher.hs
+++ cryptocipher/Crypto/Cipher.hs
@@ -1,3 +1,26 @@
+-- |
+-- Module      : Crypto.Cipher
+-- License     : BSD-style
+-- Maintainer  : Vincent Hanquez <vincent@snarc.org>
+-- Stability   : stable
+-- Portability : good
+--
+-- All the cipher functionalities are available through the
+-- BlockCipher and StreamCipher classes.
+--
+-- A simplified example (with simplified error handling):
+--
+-- > import Crypto.Cipher
+-- > import Data.ByteString (ByteString)
+-- > import qualified Data.ByteString as B
+-- >
+-- > initAES256 :: ByteString -> AES256
+-- > initAES256 = either (error . show) cipherInit . makeKey
+-- >
+-- > cbcEncryption :: AES256 -> ByteString -> ByteString -> ByteString
+-- > cbcEncryption ctx ivRaw plainText = cbcEncrypt ctx iv plainText
+-- >   where iv = maybe (error "invalid IV") id $ ivRaw
+--
 module Crypto.Cipher
     (
     -- * Cipher classes
